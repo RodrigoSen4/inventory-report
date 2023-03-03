df1e61c8a2d1c4160a12846cb1b7910a1ca929c4
Rodrigo Sena <dias.rodrigosena@gmail.com>
2023-03-03 16:23:12 -0300

    Req1 e 2

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,19 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    pass
+    product = Product(
+        id=1,
+        nome_da_empresa='Trybe',
+        nome_do_produto='Course',
+        data_de_fabricacao='03/03/2022',
+        data_de_validade='03/03/2023',
+        numero_de_serie='24',
+        instrucoes_de_armazenamento='Armazene com cuidado',
+    )
+
+    vl1 = 'O produto Course fabricado em 03/03/2022 por Trybe'
+    vl2 = 'validade até 03/03/2023 precisa ser armazenado Armazene com cuidado'
+
+    assert repr(product) == f'{vl1} {vl2}.'
