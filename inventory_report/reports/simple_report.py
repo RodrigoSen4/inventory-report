83a00dc5a356a871832d431e833f8cfa4b0c27e6
Rodrigo Sena <dias.rodrigosena@gmail.com>
2023-03-03 16:28:22 -0300

    Req1 e 2

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -15,18 +15,3 @@ class SimpleReport:
         Data de validade mais próxima: {item_list[0]['data_de_validade']}
         Empresa com mais produtos: {item_list[0]['nome_da_empresa']}
         '''
-
-
-print(SimpleReport.generate(
-     [
-         {
-             "id": 1,
-             "nome_do_produto": "CADEIRA",
-             "nome_da_empresa": "Forces of Nature",
-             "data_de_fabricacao": "2022-04-04",
-             "data_de_validade": "2023-02-09",
-             "numero_de_serie": "FR48",
-             "instrucoes_de_armazenamento": "Conservar em local fresco"
-         }
-        ]
- ))
